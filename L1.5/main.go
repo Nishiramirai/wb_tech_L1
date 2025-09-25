6c8f6df6fda3515c92abb1bc3ab2f3f537dcc41b
pavel <pavel@>
2025-09-26 02:36:27 +0300

    Finished task 5

--- L1.5/main.go
+++ L1.5/main.go
@@ -1,10 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"math/rand/v2"
+	"time"
+)
+
 // Таймаут на канал
 
 // Разработать программу, которая будет последовательно отправлять значения в канал,
 // а с другой стороны канала – читать эти значения. По истечении N секунд программа должна завершаться.
 
-func main() {
+const (
+	timeoutValue = time.Second * 5
+	sendDelay    = time.Second * 1
+)
 
+func main() {
+	ch := make(chan int)
+
+	go func() {
+		for {
+			ch <- rand.IntN(100)
+			time.Sleep(sendDelay)
+		}
+	}()
+
+	timeout := time.After(timeoutValue)
+
+	for {
+		select {
+		case v := <-ch:
+			fmt.Println(v)
+		case <-timeout:
+			// По хорошему всегда стоит делать graceful shutdown (тут горутина остается незавершенной),
+			// но мне кажется суть этого задания в том, что мы якобы ждем что-то от нас независящее
+			// и тут это не особо уместно, так как в задании рекомендуется time.After.
+			// Но можно было бы это сделать через context.WithTimeout
+			fmt.Println("Timeout")
+			return
+		}
+	}
 }
