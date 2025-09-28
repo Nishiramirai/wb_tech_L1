f0c2846dad7559f4420738b6ecfd93d27997a228
pavel <pavel@>
2025-09-28 16:34:59 +0300

    finished task 14

--- L1.14/main.go
+++ L1.14/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 // Определение типа переменной в runtime
 
@@ -9,9 +12,25 @@ import "fmt"
 //  int, string, bool, chan (канал).
 
 func main() {
-	printType("Hello")
+	// Базовые типы, которые нужно распозновать (int, string, bool)
 	printType(123)
+	printType("Hello")
 	printType(true)
+
+	// Каналы. Определяет любой тип каналов
+	chInt := make(chan int)
+	chString := make(chan string)
+	chBool := make(chan bool)
+	chSlice := make(chan []int)
+
+	printType(chInt)
+	printType(chString)
+	printType(chBool)
+	printType(chSlice)
+
+	// Пример типа, распознавание которого в задании не требовалось,
+	// Следовательно напишет Unkown type
+	printType(1.14)
 }
 
 func printType(v interface{}) {
@@ -23,6 +42,15 @@ func printType(v interface{}) {
 	case bool:
 		fmt.Println("Boolean")
 	default:
-		fmt.Println("Unknown type")
+		// Для каналов метод использующийся выше не подойдет, так как нужно указывать конкретный тип данных в канале.
+		// Можно конечно перечислить конкретные типы, но для этого потребуется много повторяющегося кода,
+		// да и все равно физически не получиться распознать каналы кастомных типов.
+		// Поэтому придется воспользоваться рефлексией
+		t := reflect.TypeOf(v)
+		if t.Kind() == reflect.Chan {
+			fmt.Printf("Chan %s\n", t.Elem())
+		} else {
+			fmt.Println("Unknown type")
+		}
 	}
 }
