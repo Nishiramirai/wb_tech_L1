218e19b95ad6f9ee6e2ad0684408ae1430052d39
pavel <pavel@>
2025-09-28 14:32:59 +0300

    Finished task 8 and 9

--- L1.8/main.go
+++ L1.8/main.go
@@ -8,23 +8,42 @@ import "fmt"
 //  Пример: для числа 5 (0101₂) установка 1-го бита в 0 даст 4 (0100₂).
 
 func main() {
-	var n int64 = 5
+	var num int64
+	var pos int64
+	var bitValue uint
 
-	bitNumber := 8
-	bitMode := 1
-
-	switch bitMode {
-	case 1:
-		n |= 1 << bitNumber
-		fmt.Println(n)
-	case 0:
-		// 0101	0100
-
-		// 0101
-		// 1011
-		// 0100
-	default:
-		fmt.Println("Error")
+	fmt.Printf("Enter number, bit position and new bit value:")
+	if _, err := fmt.Scanln(&num, &pos, &bitValue); err != nil {
+		fmt.Println("Invalid input:", err)
+		return
 	}
 
+	// В условии задания как я понимаю биты считаются с 1, а не нуля, поэтому делаю -1
+	res, err := setBit(num, pos-1, bitValue)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+
+	fmt.Printf("Source number: %d - %b\n", num, num)
+	fmt.Printf("Result: %d - %b\n", res, res)
+
+}
+
+func setBit(n int64, pos int64, value uint) (int64, error) {
+	// Проверяем позицию на вхождение в диапазон
+	if pos >= 64 || pos < 0 {
+		return 0, fmt.Errorf("invalid bit position: %d. must be in range [0, 63]", pos)
+	}
+
+	switch value {
+	case 1:
+		n |= (1 << pos)
+	case 0:
+		n &^= (1 << pos)
+	default:
+		return 0, fmt.Errorf("invalid bit value")
+	}
+
+	return n, nil
 }
--- L1.9/main.go
+++ L1.9/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Конвейер чисел
 
 // Разработать конвейер чисел. Даны два канала: в первый пишутся числа x из массива,
@@ -8,5 +10,35 @@ package main
 //  Убедитесь, что чтение из второго канала корректно завершается.
 
 func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	c := gen(arr...)
+	out := sq(c)
 
+	for n := range out {
+		fmt.Println(n)
+	}
+}
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, v := range nums {
+			out <- v
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * 2
+		}
+		close(out)
+	}()
+
+	return out
 }
