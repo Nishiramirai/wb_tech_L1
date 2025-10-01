59dc6abb3d08b3569580a4bc5ae2909ce0cd255b
pavel <pavel@>
2025-10-02 02:19:25 +0300

    finished task 19

--- L1.19/main.go
+++ L1.19/main.go
@@ -12,4 +12,16 @@ import "fmt"
 func main() {
 	var str string
 	fmt.Scan(&str)
+
+	strRev := reverseStr(str)
+	fmt.Println(strRev)
+}
+
+func reverseStr(s string) string {
+	rns := []rune(s)
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+
+	return string(rns)
 }
